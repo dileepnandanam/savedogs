b5de1de9935f9e219ababddba48f2f57448211b6
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-03-14 21:21:32 +0530

    adding letter opener gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,22 +37,12 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.perform_deliveries = true
-  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true  #config.action_mailer.delivery_method = :smtp
   #config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
   config.action_mailer.default_options = { from: 'streetdogs.in.india@gmail.co' }
-  config.action_mailer.smtp_settings  = {            
-    :address              => "smtp.gmail.com", 
-    :port                 => 465,
-    :domain               => 'localhost:3000',               
-    :user_name            => 'streetdogs.in.india@gmail.com',
-    :password             => ENV['GMAIL_PASSWORD'],         
-    :authentication       => 'plain',
-    :ssl                  => true,
-    :tls                  => true,
-    :enable_starttls_auto => true,
-  }
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
